930b4805cc65ed0ece0497151b45216ecbb5267a
Sergey Savchuk <sleepingdog.dev@gmail.com>
2023-01-08 12:48:55 +0300

    New wheat texture

--- lib/wheat_tile.dart
+++ lib/wheat_tile.dart
@@ -1,14 +1,13 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
-import 'package:flame_forge2d/body_component.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flametest/harvester.dart';
 import 'package:flametest/harvester_game.dart';
-import 'package:forge2d/src/dynamics/body.dart';
 
 class WheatTile extends BodyComponent<HarvesterGame> with ContactCallbacks {
-  final _baseSize = Vector2(1.0, 1.0);
+  final _baseSize = Vector2(1.0, 1.16);
+  final _bottomRadius = 0.5;
 
   bool collected = false;
 
@@ -21,10 +20,11 @@ class WheatTile extends BodyComponent<HarvesterGame> with ContactCallbacks {
     await super.onLoad();
 
     add(SpriteComponent(
-        anchor: Anchor.center,
+        anchor: Anchor.bottomCenter,
         // make size a tiny bit larger so there is no gap between tiles
         size: _baseSize + Vector2(0.01, 0.01),
-        sprite: await gameRef.loadSprite('dummy_wheat.jpg')));
+        position: Vector2(0, _bottomRadius),
+        sprite: await gameRef.loadSprite('cute_wheat.png')));
   }
 
   @override
@@ -35,7 +35,7 @@ class WheatTile extends BodyComponent<HarvesterGame> with ContactCallbacks {
       type: BodyType.static,
     );
 
-    final shape = CircleShape()..radius = 0.5;
+    final shape = CircleShape()..radius = _bottomRadius;
     final fixtureDef = FixtureDef(shape)..isSensor = true;
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
